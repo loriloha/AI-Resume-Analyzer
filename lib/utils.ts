c01e28d66014271ed86b58c98bd66deb3325fa49
loriloha <loriloha31@gmail.com>
2025-12-03 13:54:05 +0300

    Cloud storage, PDF-Image conversion, key-value storage and AI feedback

---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+export default function formatSize(bytes: number): string {
+    if (bytes === 0 ) return '0 Bytes';
+    const k= 1024;
+    const sizes:string[]= ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+    const i:number = Math.floor(Math.log(bytes)/Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+}
+
+export const generateUUID = ()=>crypto.randomUUID();
